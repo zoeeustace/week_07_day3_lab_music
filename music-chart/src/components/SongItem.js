7ddf37fdaf6a0f7644d4c8cdbc39d66b8ac084a1
Zoe Eustace <zoeeustace@zoes-air.home>
2023-05-03 19:29:44 +0100

    completed with music preview ext

--- music-chart/src/components/SongItem.js
+++ music-chart/src/components/SongItem.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const SongItem = ({song}) => {
     return <li>{song.title.label} <img id="album-img" src={song["im:image"][0].label}/>
-    {/* <button onClick={handleClickPlay} src={song["link"][1].attributes}/> */}
+        <audio controls>
+        <source src={song["link"][1]["attributes"]["href"]} type="audio/mp4"></source>
+    </audio>
     </li>;
 }
  
